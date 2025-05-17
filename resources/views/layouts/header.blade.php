f543cfe35f3f327c9902f243cc75134d2385a16f
Chheng Seyha <seyhachheng26@gmail.com>
2025-05-17 11:26:53 +0700

    Added DahsboardController and menu depand on user role

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -138,13 +138,14 @@
           <img src="{{asset('assets/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name}}</a>
         </div>
       </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          @if(Auth::user()->user_type == 1)
           <li class="nav-item">
             <a href="{{url('admin/dashboard')}}" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -158,6 +159,43 @@
               <i class="nav-icon far fa-user"></i>
               <p>
                 Admin
+              </p>
+            </a>
+          </li>
+          @elseif(Auth::user()->user_type == 2)
+          <li class="nav-item">
+            <a href="{{url('admin/dashboard')}}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          @elseif(Auth::user()->user_type == 3)
+          <li class="nav-item">
+            <a href="{{url('admin/dashboard')}}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          @elseif(Auth::user()->user_type == 4)
+          <li class="nav-item">
+            <a href="{{url('admin/dashboard')}}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          @endif
+          
+           <li class="nav-item">
+            <a href="{{url('/logout')}}" class="nav-link">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+                Logout
               </p>
             </a>
           </li>
